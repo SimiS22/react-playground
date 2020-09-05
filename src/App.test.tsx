672c3c262f16192ee0b8aad9bb809db852e438c6
Simi Swaminathan <simiswaminathan@gmail.com>
2020-09-05 22:47:56 +0200

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,20 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import { TestApp } from './App';
 
 describe('test', () => {
-  it('test 1', async () => {
-    render(<TestApp />);
+  it('test 1', () => {
+    const rendered = render(<TestApp />);
 
-    const myDiv = await screen.findByTestId('myDiv');
-    const myButton = await screen.findByTestId('myButton');
+    const myDiv = rendered.getByTestId('myDiv');
+    const myButton = rendered.getByTestId('myButton');
 
     expect(myDiv).toHaveTextContent('initial value');
 
     fireEvent.click(myButton);
 
     expect(myDiv).toHaveTextContent('final value');
+
+    expect(rendered.container).toMatchSnapshot('final state')
   });
 });
